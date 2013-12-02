84d4a03bdc906006d1b23d5f46c3dfde3d21dc00
Curtis Larson <quackware@gmail.com>
2013-12-01 21:03:56 -0500

    Added a chunk read process method

--- soundcloud-scraper.py
+++ soundcloud-scraper.py
@@ -87,9 +87,25 @@ def download(trackUrl, title, outputDirectory):
 	print("Downloading " + title)
 
 	trackFile = urllib2.urlopen(trackUrl)
+
+
 	data = trackFile.read()
 	with open (outputDirectory + "/" + title + ".mp3", "wb") as mp3:
 		mp3.write(data)
+
+def chunkRead(response, chunk_size=8192, report_hook=None):
+	total_size = response.info().getHeader('Content-Length').strip()
+	total_size = int(total_size)
+	bytes_so_far = 0
+
+	while 1:
+		chunk = response.read(chunk_size)
+		bytes_so_far += len(chunk)
+		if not chunk:
+			break
+		if report_hook:
+			report_hook(bytes_so_far, chunk_size, total_size)
+	return bytes_so_far
 
 def printUsage():
 	print("soundcloud-scraper.py -t <tag> -n <number> -o <outputDirectory> "
