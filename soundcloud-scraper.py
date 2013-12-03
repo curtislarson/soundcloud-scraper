3fde7ef62e2d5d8b388877c762905581712a67f1
Curtis Larson <quackware@gmail.com>
2013-12-02 19:01:47 -0500

    Finished adding download progress indicator

--- soundcloud-scraper.py
+++ soundcloud-scraper.py
@@ -87,24 +87,33 @@ def download(trackUrl, title, outputDirectory):
 	print("Downloading " + title)
 
 	trackFile = urllib2.urlopen(trackUrl)
+	chunkRead(trackFile,
+		outputDirectory + "/" + title + ".mp3",
+		report_hook=chunkReport)
 
+def chunkReport(bytes_so_far, chunk_size, total_size):
+	percent = float(bytes_so_far) / total_size
+	percent = round(percent * 100, 2)
+	sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" % 
+       (bytes_so_far, total_size, percent))
 
-	data = trackFile.read()
-	with open (outputDirectory + "/" + title + ".mp3", "wb") as mp3:
-		mp3.write(data)
+   	if bytes_so_far >= total_size:
+   		sys.stdout.write('\n')
 
-def chunkRead(response, chunk_size=8192, report_hook=None):
-	total_size = response.info().getHeader('Content-Length').strip()
+def chunkRead(response, output, chunk_size=8192, report_hook=None):
+	total_size = response.info().getheader('Content-Length').strip()
 	total_size = int(total_size)
 	bytes_so_far = 0
 
 	while 1:
-		chunk = response.read(chunk_size)
-		bytes_so_far += len(chunk)
-		if not chunk:
-			break
-		if report_hook:
-			report_hook(bytes_so_far, chunk_size, total_size)
+		with open (output, "ab") as mp3:
+			chunk = response.read(chunk_size)
+			mp3.write(chunk)
+			bytes_so_far += len(chunk)
+			if not chunk:
+				break
+			if report_hook:
+				report_hook(bytes_so_far, chunk_size, total_size)
 	return bytes_so_far
 
 def printUsage():
